19b94bb1a48dd2fd8ca478189c6f519069234b4c
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-03-20 15:15:37 -0300

    feat: category page

--- app/Category/[category]/page.tsx
+++ app/Category/[category]/page.tsx
@@ -11,6 +11,7 @@ import { RiSurroundSoundLine } from "react-icons/ri";
 
 export default function Category({ params }: { params: { category: string } }) {
     const [categoryList, setCategoryList] = useState<Product[]>([]);
+    const [categoryListFiltered, setCategoryListFiltered] = useState<Product[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,36 +22,55 @@ export default function Category({ params }: { params: { category: string } }) {
     }, []);
 
     let icon;
+    let categoryID:string = ""
 
     if (params.category === 'mouses') {
         icon = <FaMouse/>;
+        categoryID = "c6911b3d-988f-4fee-80d3-fda966c75c76"
     } else if (params.category === 'keyboards') {
         icon = <FaRegKeyboard/>;
+        categoryID = "d41ade5e-514d-4c10-b9c4-53edff445c93"
     } else if (params.category === 'headphones') {
         icon = <FaHeadset/>;
+        categoryID = "a97f5749-839a-4e8a-adbe-4ecba77b79db"
     } else if (params.category === 'mousepads') {
         icon = <FaRegSquare/>;
+        categoryID = "9e7d953d-93c0-49dd-8095-bd2633623a1e"
     } else if (params.category === 'monitors') {
         icon = <MdMonitor/>;
+        categoryID = "c4f795e6-eb5d-47cc-9aa1-7ff6d3d28cd1"
     } else if (params.category === 'speakers') {
         icon = <RiSurroundSoundLine/>;
+        categoryID = "212de033-516d-4f76-80a7-98cf4f19c91f"
     } 
+
+    useEffect(()=>{
+        let filtered = categoryList.filter((item)=>{
+            return item.categoryId === categoryID
+        })
+        setCategoryListFiltered(filtered)
+
+    },categoryList)    
 
     return (
         <>
             <div className="container mx-auto">
+                <div className="flex w-10/12 flex-wrap mx-auto">
                 <div className="border-2 w-[200px] h-[45px] flex rounded-3xl border-purple-600 my-9 justify-center">
                     <div className="flex items-center w-8/12 justify-between">
                     {icon}
                     <p>{params.category.toUpperCase()}</p>
                     </div>
                 </div>
+                </div>
+                <div className="flex w-10/12 flex-wrap mx-auto">
                 {
-                    categoryList.map((item: Product) => {
+                    categoryListFiltered.map((item: Product) => {
                         return <ProdItem id={item.id} discount={item.discountPercentage} name={item.name} image={item.imageUrls} price={Number(item.basePrice)} />
                     })
 
                 }
+                </div>
             </div>
 
 
--- app/components/ProdItem/ProdItem.tsx
+++ app/components/ProdItem/ProdItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Product } from "@prisma/client";
 import Image from "next/image";
+import { FaArrowDownLong } from "react-icons/fa6";
+import { FaPercentage } from "react-icons/fa";
 
 type prod = {
     id: string;
@@ -10,24 +12,29 @@ type prod = {
     price: number;
 }
 
-export default function ProdItem(props:prod) {
+export default function ProdItem(props: prod) {
     return (
         <>
-        <div>
-        <p>{props.discount}</p>
-        <img src={props.image[0]}></img>
-        <img src={props.image[0]}></img>
-        <Image src={props.image[0]} alt="test" width="200" height="200"></Image>
-        <p>{props.name}</p>
-        <p>{props.name}</p>
-
-
-            <p>{props.name}</p>
-            <p>R$ {props.price}</p>
-            <p>R$ {props.price}</p>
-
-
-
+            <div className="w-[250px] m-6">
+                <div className="bg-slate-900 w-[250px] h-[250px] items-center rounded-2xl">
+                    <div className="w-[70px] h-[30px] flex items-center bg-purple-700 justify-center absolute mt-3 ml-3 rounded-2xl">
+                        <div className="flex items-center justify-around w-9/12">
+                            <FaArrowDownLong />
+                            <p>{props.discount}</p>
+                            <FaPercentage />
+                        </div>
+                    </div>
+                    <div className="w-9/12 mx-auto pt-[10%]">
+                        <img src={props.image[0]} alt="test" className="m-auto my-auto max-w-full"></img>
+                    </div>
+                </div>
+                <div className="mt-1">
+                    <h1 className="text-lg">{props.name}</h1>
+                    <div className="flex items-center justify-between w-7/12">
+                        <p className="font-bold	text-xl">R$ {props.price}</p>
+                        <p className="line-through">R$ {props.price}</p>
+                    </div>
+                </div>
             </div>
         </>
     );
