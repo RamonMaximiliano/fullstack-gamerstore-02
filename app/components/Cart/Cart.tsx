18d7f57c43d7e0c45fc31b6cb31f64ff92c4f83d
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-05-08 17:57:25 -0300

    feat: cart continue

--- app/components/Cart/Cart.tsx
+++ app/components/Cart/Cart.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 
 export default function Cart() {
-   
+
     return (
         <>
             <div className="bg-gray-800">
-                    cart
+                <div className="flex w-8/12 flex-wrap mx-auto mt-8">
+                    <div className="border-2 w-[200px] h-[45px] flex rounded-3xl border-purple-600 my-4 justify-center">
+                        <div className="flex items-center w-8/12 justify-between">
+                       {/*      <MdOutlineDiscount size={18} className="mr-3" /> */}
+                            <p>Descontos</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
